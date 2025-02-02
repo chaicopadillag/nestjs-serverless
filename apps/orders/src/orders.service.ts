c77e446f123ff03b50d9682daaedde109ca629b7
chaicopadillag <xt9738@rimac.com.pe>
2025-02-02 08:50:15 -0500

    feat: integrate SQS for order processing; update OrdersService to send messages to the pending orders queue

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,19 +1,50 @@
+import {
+  SQSClient,
+  SendMessageCommand,
+  SendMessageCommandInput,
+} from '@aws-sdk/client-sqs';
 import { Injectable } from '@nestjs/common';
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+const sqsClient = new SQSClient({ region: process.env.APP_REGION });
 
 @Injectable()
 export class OrdersService {
   async createOrder(req: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
-    const body = JSON.parse(req.body);
+    try {
+      const body = JSON.parse(req.body);
 
-    const newOrder = {
-      id: Date.now(),
-      ...body,
-    };
+      const newOrder = {
+        id: Date.now(),
+        ...body,
+      };
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(newOrder),
-    };
+      const params: SendMessageCommandInput = {
+        QueueUrl: process.env.PENDING_ORDERS_QUEUE_URL,
+        MessageBody: JSON.stringify(newOrder),
+      };
+
+      const orderMessageSender = new SendMessageCommand(params);
+      const sqsOrderDeliveryResult = await sqsClient.send(orderMessageSender);
+
+      console.log(
+        '✅ Order sent to SQS',
+        JSON.stringify(sqsOrderDeliveryResult),
+      );
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(newOrder),
+      };
+    } catch (error) {
+      console.error('❌ Error creating order', error);
+
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: 'Error creating order',
+        }),
+      };
+    }
   }
 }
